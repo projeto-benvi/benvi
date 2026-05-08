8c765974f7d87e93139618d2fdf00265c25da10c
Joao Marcos <joaom02@outlook.com>
2026-05-08 15:06:43 -0300

    Subindo Branch

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const fontBenvi = Inter({subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: "Benvi",
+  description: "Plataforma que conecta pessoas a profissionais confiáveis de forma rápida, simples e segura.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-br"
+      className={`${fontBenvi} h-full antialiased`}
+    >
+      <body>
+        
+        {children}
+      </body>
+    </html>
+  );
+}
