197e968ba0348fe6f2af12563936e147a404b918
dungeonnemesis <toniviniciuspaixao@gmail.com>
2026-05-08 14:38:28 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const fontBenvi = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: "Benvi",
+  title: "oi",
   description: "Plataforma que conecta pessoas a profissionais confiáveis de forma rápida, simples e segura.",
 };
 
