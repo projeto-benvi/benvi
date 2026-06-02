14913305f2cbfd12b271262e113ed6bed207d3bb
Camile Lima <camilea.lima@gmail.com>
2026-06-02 13:44:38 -0300

    construtor usuario
    mudei o model para classe e fiz o construtor

--- model/usuario.ts
+++ model/usuario.ts
@@ -1,4 +1,4 @@
-export interface Usuario {
+export class Usuario {
   id_usuario?: number;
   nome: string;
   email: string;
@@ -12,4 +12,34 @@ export interface Usuario {
   status_conta?: string;
   data_criacao?: Date;
   is_admin?: boolean;
+
+  constructor(dados: {
+    id_usuario?: number;
+    nome: string;
+    email: string;
+    senha: string;
+    telefone?: string;
+    foto_perfil?: string;
+    cpf: string;
+    data_nascimento: Date;
+    cidade?: string;
+    nivel_acesso?: number;
+    status_conta?: string;
+    data_criacao?: Date;
+    is_admin?: boolean;
+  }) {
+    this.id_usuario = dados.id_usuario;
+    this.nome = dados.nome;
+    this.email = dados.email;
+    this.senha = dados.senha;
+    this.telefone = dados.telefone;
+    this.foto_perfil = dados.foto_perfil;
+    this.cpf = dados.cpf;
+    this.data_nascimento = dados.data_nascimento;
+    this.cidade = dados.cidade;
+    this.nivel_acesso = dados.nivel_acesso ?? 1;
+    this.status_conta = dados.status_conta ?? 'ativo';
+    this.data_criacao = dados.data_criacao;
+    this.is_admin = dados.is_admin ?? false;
+  }
 }
