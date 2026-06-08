4f5da6c4b3fc2159b59e2b3c4195256550c710f7
Romulo <romulogmelo@gmail.com>
2026-06-08 14:48:38 -0300

    Fazendo merge da main em minha branch

--- service/categoriaService.ts
+++ service/categoriaService.ts
@@ -1,32 +1,16 @@
-import  pool  from '@/app/lib/dataBase';
-export class CategoriaService {
-  
-  async listarTodasCategorias() {
-    const query = `SELECT id_categoria, nome_categoria FROM CATEGORIA`;
-    return await pool.query(query);
-  }
+import pool from '@/app/lib/dataBase';
 
-  async listarCardsServicos(idCategoriaFiltro?: string) {
-    let query = `
-      SELECT 
-          s.id_servico,
-          s.titulo,
-          s.descricao,
-          c.nome_categoria,
-          COUNT(s.id_prestador) AS profissionais_disponiveis
-      FROM SERVICO s
-      INNER JOIN CATEGORIA c ON s.id_categoria = c.id_categoria
+export class CategoriaService {
+
+  async listarTodasCategorias() {
+    const query = `
+      SELECT id_categoria, nome_categoria
+      FROM CATEGORIA
     `;
 
-    const params: (string | number | null)[] = [];
+    const [rows] = await pool.query(query);
 
-    if (idCategoriaFiltro) {
-      query += ` WHERE c.id_categoria = ? `;
-      params.push(idCategoriaFiltro);
-    }
-
-    query += ` GROUP BY s.id_servico, s.titulo, s.descricao, c.nome_categoria`;
-
-    return await pool.query(query, params);
+    return rows;
   }
+
 }
