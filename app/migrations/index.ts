0f5e5c8ab6eac8fc10a7763874cf0cc40916321f
Camile Lima <camilea.lima@gmail.com>
2026-06-16 16:16:19 -0300

    Update index.ts

--- app/migrations/index.ts
+++ app/migrations/index.ts
@@ -7,7 +7,7 @@ import { up as createServico } from './004_create_servico';
 import { up as createSolicitacaoServico } from './008_create_solicitacaoServico';
 import { up as createAgenda } from './009_create_agenda';
 import { up as createAvaliacao } from './010_create_avaliacao';
-import { up as createNotificacaoServico } from './013_create_notificacaoServico';
+
 import { up as createConversa } from './015_create_conversa';
 import { up as createConversaMensagem } from './016_create_mensagem';
 import { up as creatAssinaturaPlano } from './018_assinaturaPlano';
@@ -21,7 +21,6 @@ const migrations = [
   { name: '008_create_SolicitacaoServico', fn: createSolicitacaoServico },
   { name: '009_create_agenda', fn: createAgenda },
   { name: '010_create_avaliacao', fn: createAvaliacao },
-  { name: '013_create_notificacaoServico', fn: createNotificacaoServico },
   { name: '015_create_conversa', fn: createConversa },
   { name: '016_create_mensagem', fn: createConversaMensagem },
   { name: '018_create_assinaturaPlano', fn: creatAssinaturaPlano },
