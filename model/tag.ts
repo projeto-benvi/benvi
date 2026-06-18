f9e07063cfedd282e90b06b45ca560fd936599ce
Romulo <romulogmelo@gmail.com>
2026-06-18 14:55:52 -0300

    remove método getCreateTableSQL da classe Tag e adiciona migração para criação da tabela tag

--- model/tag.ts
+++ model/tag.ts
@@ -13,15 +13,5 @@ export class Tag {
     this.id_prestador = dados.id_prestador;
   }
 
-  static getCreateTableSQL(): string {
-    return `
-    CREATE TABLE IF NOT EXISTS tag (
-    id_tag INT AUTO_INCREMENT PRIMARY KEY,
-    id_categoria INT,
-    id_prestador INT,
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
-    FOREIGN KEY (id_prestador) REFERENCES prestador(id_usuario)
-    );
-    `;
-}
+ 
 }
